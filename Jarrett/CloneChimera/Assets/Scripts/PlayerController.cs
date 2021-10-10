01c9344a79fdb11d229a71bb1bbc4ffe1ba347e7
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2021-10-09 23:08:41 -0700

    asteroids
    player mov
    player fire
    asteroids mov

---
+++ Jarrett/CloneChimera/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotSpeed;
+    public float moveSpeed;
+    public GameObject laser;
+    public float cooldown = 1f;
+    private float time = 0f;
+    private Rigidbody2D rb;
+
+    public float laserSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0f){
+            time -= Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.Space)){
+            GameObject laserObj = Instantiate(laser, transform.TransformPoint(Vector3.forward * 2), transform.rotation);
+            laserObj.GetComponent<Rigidbody2D>().velocity = transform.up * laserSpeed * Time.deltaTime;
+            time = cooldown;
+        }
+
+        if (Input.GetKey(KeyCode.W)){
+            // GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed * Time.deltaTime);
+            // Debug.Log("wokring");
+            rb.velocity = transform.up * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+            // GetComponent<Rigidbody>().AddForce(transform.forward * -moveSpeed * Time.deltaTime);
+            rb.velocity = transform.up * -moveSpeed * Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(-transform.forward * rotSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(transform.forward * rotSpeed * Time.deltaTime);
+    }
+}
---
+++ Jarrett/CloneChimera/Assets/Scripts/asteroidController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Vector3 randomDirection = new Vector3(Random.value, Random.value);
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
