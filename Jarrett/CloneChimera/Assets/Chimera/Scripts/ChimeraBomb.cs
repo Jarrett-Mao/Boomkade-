45932e16632f46e2d92795a427fec4d71dc65b80
GitHusbando <67436617+GitHusbando@users.noreply.github.com>
2021-10-12 15:28:39 -0700

    Added bomb and explosion prefabs with scripts.

---
+++ Jarrett/CloneChimera/Assets/Chimera/Scripts/ChimeraBomb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimeraBomb : MonoBehaviour
+{
+    public ChimeraExplosion chimeraExplosion;
+
+    void Awake()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    void OnDestroy()
+    {
+        Explode();
+    }
+
+    void Explode()
+    {
+        Destroy(gameObject); //calling destroy inside the OnDestroy function seems to work just fine
+        Instantiate(chimeraExplosion, gameObject.transform.position, Quaternion.identity);
+    }
+}
---
+++ Jarrett/CloneChimera/Assets/Chimera/Scripts/ChimeraExplosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimeraExplosion : MonoBehaviour
+{
+    public Player player;
+    public Cargo cargo;
+    static float SCALE_MULTIPLIER = 2f;
+
+    void Awake()
+    {
+        DestroyColliders();
+        Destroy(gameObject, 0.2f);
+    }
+
+    void DestroyColliders()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(gameObject.transform.position, transform.localScale.x * SCALE_MULTIPLIER);
+        foreach (var col in colliders)
+        {
+            if (col.tag == "PlayerShip")
+            {
+                GameManager.instance.HandleDestroyPlayer(col.GetComponent<Player>());
+            }
+            else if (col.tag == "Wall")
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    }
+}
