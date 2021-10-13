4e3f67335cf18df03932183b2459c357c8990694
DLBrab <bimcdona@ucsc.edu>
2021-10-13 00:50:07 -0700

    Bomb now flickers before exploding + Updated art asset.
    Weird issues with starting the game, but I'll get into testing why that is tomorrow. Might just be something on my end.

--- Jarrett/CloneChimera/Assets/Scripts/Bomb.cs
+++ Jarrett/CloneChimera/Assets/Scripts/Bomb.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public GameObject explodepf;
+    SpriteRenderer sprite;
+    double fuse = 3.0f;
+    double blinkTimer = 2.7f;
+    Color spriteColor;
+
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         StartCoroutine(explode());
+        spriteColor = sprite.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fuse -= Time.deltaTime;
+        if(fuse > 0.05f && fuse < blinkTimer){
+            Debug.Log("Bomb is blinking!");
+            Color.RGBToHSV(sprite.color, out float H, out float S, out float V);
+            float negativeHue = (H + 0.5f) % 1f;
+            sprite.color = Color.HSVToRGB(negativeHue, S, V);
+            sprite.color = spriteColor;
+            blinkTimer = 0.9 * fuse;
+        }
     }
 
     IEnumerator explode(){
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitUntil(() => (fuse <= 0));
         Instantiate(explodepf, transform.position, explodepf.transform.rotation);
         Destroy(this.gameObject);
     }
