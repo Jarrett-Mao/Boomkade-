9ca07b1c64df6c1fbb5ae1e53872db909d143b7b
DLBrab <bimcdona@ucsc.edu>
2021-10-13 18:24:01 -0700

    Bomb blinks red faster as it approaches detonation
    Would like to do negative, but would probably require me to shimmy with another sprite and that's not very modular. Function that I found does not work, at least not for 2D sprites.

--- Jarrett/CloneChimera/Assets/Chimera/Scripts/ChimeraBomb.cs
+++ Jarrett/CloneChimera/Assets/Chimera/Scripts/ChimeraBomb.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class ChimeraBomb : MonoBehaviour
 {
     public ChimeraExplosion chimeraExplosion;
+    SpriteRenderer sprite;
+    double fuse = 3.0f;
+    double blinkTimer = 2.7f;
+    Color spriteColor;
+    Color red = new Color (1, 0, 0, 1);
 
-    void Awake()
-    {
-        Destroy(gameObject, 3);
+    void Start(){
+        sprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(explode());
+        spriteColor = sprite.color;
     }
 
-    void OnDestroy()
-    {
-        Explode();
+    void Update(){
+        fuse -= Time.deltaTime;
+        if(fuse > 0.05f && fuse < blinkTimer){
+            Debug.Log("Bomb is blinking!");
+            /* 
+            // Code intends to turn the bomb to its negative color. Broken.
+            Color.RGBToHSV(sprite.color, out float H, out float S, out float V);
+            float negativeHue = (H + 0.5f) % 1f;
+            sprite.color = Color.HSVToRGB(negativeHue, S, V); 
+            */
+            sprite.color = (sprite.color == red) ? spriteColor : red;
+            blinkTimer = 0.9 * fuse;
+        }
     }
 
-    void Explode()
-    {
-        Destroy(gameObject); //calling destroy inside the OnDestroy function seems to work just fine
+    IEnumerator explode(){
+        yield return new WaitUntil(() => (fuse <= 0));
         Instantiate(chimeraExplosion, gameObject.transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
     }
 }
